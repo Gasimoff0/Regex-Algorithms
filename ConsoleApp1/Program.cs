1bd21f26324e53ddd48f9738e868f04ead66964d
Gasimoff0 <mddska.01@icloud.com>
2025-04-30 12:45:44 +0400

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿
+#region task1
+//int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+//string tekler = "";
+//string cutler = "";
+
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (array[i] % 2 == 0)
+//    {
+//        cutler += array[i] + ", ";
+//    }
+//    else
+//    {
+//        tekler += array[i] + ", ";
+//    }
+//}
+
+//Console.WriteLine("Tek ededler: " + tekler);
+//Console.WriteLine("Cut ededler: " + cutler);
+#endregion
+
+#region task2
+//int[] array = { 2, 4, 6, 8, 10 };
+//int cutIndxSum = 0;
+//int tekIndxSum = 0;
+
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (i%2==0)
+//    {
+//        cutIndxSum += array[i];
+//    }
+//    else
+//    {
+//        tekIndxSum += array[i];
+//    }
+//}
+//int ferq = cutIndxSum - tekIndxSum;
+//Console.WriteLine("Cut index cemi: " + cutIndxSum);
+//Console.WriteLine("Tek index cemi: " + tekIndxSum);
+//Console.WriteLine("Ferqi: " + ferq);
+#endregion
+
+#region task3
+//int[] array = { 1, 2, 3, 2, 4, 1, 5 };
+
+//Console.WriteLine("Tekrarlanmayan ededler: ");
+
+//for (int i = 0; i < array.Length; i++)
+//{
+//    int say = default;
+
+//    for (int j = 0; j < array.Length; j++)
+//    {
+//        if (array[i] == array[j])
+//        {
+//            say++;
+//        }
+//    }
+
+//    if (say == 1)
+//    {
+//        Console.Write(array[i]+ ",");
+//    }
+//}
+#endregion
